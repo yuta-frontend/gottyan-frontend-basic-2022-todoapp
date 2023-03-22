7b01f9bc9147f4fc4e4321da91efe799a0be507d
Gottyan <y.goto.22.8.608@4nonome.com>
2023-03-22 15:15:36 +0900

    Initial commit

---
+++ src/variables/color.js
@@ -0,0 +1,10 @@
+const COLOR = {
+  BLACK: "#232324",
+  LIGHT_BLACK: "#2B2A2E",
+  DEEP_BLUE_GRAY: "#2F3136",
+  LIGHT_GRAY: "#B8B8B8",
+  WHITE: "#FFFFFF",
+  GREEN: "#46A381",
+  RED: "#F0947F",
+};
+export default COLOR;
