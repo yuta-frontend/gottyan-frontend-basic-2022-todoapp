cf6feb9c8c0eeb1652cda8200a6016ef94085789
gottyan <ygt5053@gmail.com>
2023-03-26 17:15:29 +0900

    styled-componentの修正

--- src/components/Atoms/EditButton/index.jsx
+++ src/components/Atoms/EditButton/index.jsx
@@ -8,25 +8,41 @@ import PENCIL from "../../../assets/svg/pencil.svg";
 const EditButton = ({ onClick }) => {
   return (
     <StyledEditButton onClick={onClick}>
-      <img src={PENCIL} className="pencil" />
+      <Circle />
+      <Img src={PENCIL} />
     </StyledEditButton>
   );
 };
 export default EditButton;
+
 const StyledEditButton = styled.button`
+  position: relative;
   background-color: transparent;
   border: 0;
   padding: 0;
   width: 20px;
   height: 20px;
   cursor: pointer;
+`;
+const Circle = styled.div`
+  position: absolute;
+  background-color: ${COLOR.LIGHT_GRAY};
+  border-radius: 50%;
+  opacity: 0;
+  top: 0px;
+  left: 0px;
+  width: 20px;
+  height: 20px;
+  transition: 0.2s ease;
+  z-index: 1;
   &:hover {
-    background-color: rgba(184, 184, 184, 0.2); //COLOR.LIGHT_GLAY
-    border-radius: 50%;
+    opacity: 0.2;
   }
-
-  .pencil {
-    width: 20px;
-    height: 20px;
-  }
+`;
+const Img = styled.img`
+  position: absolute;
+  top: 0px;
+  left: 0px;
+  width: 20px;
+  height: 20px;
 `;
