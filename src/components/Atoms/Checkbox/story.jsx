33ed0ea013aa9d8e752a2337ab21e66682303fee
gottyan <ygt5053@gmail.com>
2023-03-28 15:13:09 +0900

    Atoms/Checkbox first commit

---
+++ src/components/Atoms/Checkbox/story.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Component from "./index";
+
+export default {
+  component: Component,
+  title: "Checkbox",
+  parameters: {
+    backgrounds: {
+      default: "dark",
+    },
+  },
+};
+
+const Template = (args) => <Component {...args} />;
+
+export const Default = Template.bind({});
